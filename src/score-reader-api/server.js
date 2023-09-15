757951150fbbac0aabd89023cf1f646cc99b9e36
Faruk Abdo Feres <faruk@mottu.com.br>
2023-09-14 21:42:42 -0300

    criado leitura dos registros no influx db

--- src/score-reader-api/server.js
+++ src/score-reader-api/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config({ path: `.env.local` })
-const express = require("express");
-const bodyParser = require('body-parser');
-const eventService = require("./services/auth-event-service");
+const express = require("express"); 
+const influxContext = require("./services/influx-context");
  
 const app = express(); 
  
-app.get("/:userlogin", (req, res) => { 
-  res.send(req.params.userlogin);
+app.get("/:userlogin", async (req, res) => { 
+  let data = await influxContext.get(req.params.userlogin);
+  res.send(data);
 });
  
-app.listen(3005, () => console.log("Register Api is listening on port 3005."));
+app.listen(3005, () => console.log("Score Reader API is listening on port 3005."));
--- src/score-reader-api/services/influx-context.js
+++ src/score-reader-api/services/influx-context.js
@@ -1,13 +1,67 @@
-const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+const { InfluxDB } = require("@influxdata/influxdb-client");
 
 const token = process.env.INFLUXDB_TOKEN;
-const url = `http://${process.env.INFLUX_HOST}:${process.env.INFLUX_PORT}`; 
-const precision = "ns";
+const url = `http://${process.env.INFLUX_HOST}:${process.env.INFLUX_PORT}`;
 
-const client = new InfluxDB({ url, token });
+const generateClient = () => {
+  return new InfluxDB({
+    url: url,
+    token: token,
+  });
+};
 
-exports.measurements = { commits: "commits", pullrequests: "pullrequests", test: "test" };
+const generateQuery = (userlogin) => {
+  return `
+    from(bucket: "${process.env.INFLUX_BUCKETNAME}")
+        |> range(start: -30d)
+        |> filter(fn: (r) => r._measurement == "github_activity" and r.github_user == "${userlogin}")
+        |> aggregateWindow(every: 1d, fn: sum, createEmpty: false)
+        |> yield(name: "sum")
+    `;
+};
+
+const getDataFromUserLogin = async (userlogin) => {
+  const client = generateClient();
+  const queryApi = client.getQueryApi(process.env.INFLUX_ORG);
+  const query = generateQuery(userlogin);
+  let data = [];
+  for await (const { values, tableMeta } of queryApi.iterateRows(query)) {
+    const _values = tableMeta.toObject(values); 
+    data.push(_values);
+  }
+
+  const contributionsByDate = {
+    userlogin: userlogin,
+    contributions: [],
+  };
+
+  // Process the query result
+  data.forEach((row, tableMeta) => {
+    const timestamp = row["_time"];
+    const fieldName = row["_field"];
+    const value = row["_value"];
+
+    // Find the entry for the current date or create a new one
+    const dateEntry = contributionsByDate.contributions.find(
+      (entry) => entry.date === timestamp
+    );
+
+    if (dateEntry) {
+      // Update the existing entry
+      dateEntry[fieldName] = value;
+    } else {
+      // Create a new entry for the current date
+      const newEntry = {
+        date: timestamp,
+        [fieldName]: value,
+      };
+      contributionsByDate.contributions.push(newEntry);
+    }
+  });
+
+  return contributionsByDate;
+};
 
 exports.get = async (userlogin) => {
-
-}
+  return await getDataFromUserLogin(userlogin);
+};
